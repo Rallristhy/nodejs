577ed68190c52f9149a7b9435d114ccc8f5c643c
rallristhy <rallristhy@outlook.com>
2017-01-31 16:30:51 -0300

    teste bower

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+/* Importando express */
 const express = require ('express');
+const bodyParser = require ('body-parser');
 const app = express();
+
+/* app usara o bodyParser
+ * use interpreta as requisições é um midleware
+ */
+app.use(bodyParser.urlencoded({extended : true}));
+
+/* Arquivos de visão terão a extensão ejs */
+app.set('view engine', 'ejs');
 
 app.get ('/', function (request, response){
   response.render ('index.ejs');
