b78eadf8cda243a9a29b20791ba7e0da47b90477
rallristhy <rallristhy@outlook.com>
2017-02-08 17:31:17 -0300

    aplicando css e criando paginacao

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var fs = require('fs');
 
 /* Mapeando caminhos para visibilidade nas views */
 app.use("/bower_components",  express.static(__dirname + '/bower_components'));
+app.use("/node_modules",  express.static(__dirname + '/node_modules'));
 app.use("/views",  express.static(__dirname + '/views'));
 
 class bovespaHeader {
@@ -20,38 +21,50 @@ class bovespaHeader {
 };
 
 class bovespaCotacao {
-	constructor(c_tipo_registro, c_data_do_pregao, c_codigo_bdi, c_codigo_de_negociacao_do_papel, c_tipo_de_mercado, c_nomres, c_especificacao_do_papel, c_prazot, c_moeda_de_referencia, c_preabe, c_premax, c_premin, c_premed, c_preult, c_preofc, c_preofv, c_totneg, c_quatot, c_voltot, c_preexe, c_idopc, c_datven, c_fatcot, c_ptoexe, c_codisi, c_dismes) {
-	this.c_tipo_registro = c_tipo_registro;
-	this.c_data_do_pregao = c_data_do_pregao;
-	this.c_codigo_bdi = c_codigo_bdi;
+	constructor(c_tipo_registro, c_data_do_pregao, c_codigo_bdi, c_codigo_de_negociacao_do_papel, c_tipo_de_mercado, c_nomres, c_especificacao_do_papel, c_prazot, c_moeda_de_referencia, c_preabe, c_premax, c_premin, c_premed, c_preult, c_preofc, c_preofv, c_totneg, c_quatot, c_voltot, c_preexe, c_idopc , c_datven, c_fatcot, c_ptoexe, c_codisi, c_dismes) {
+	this.c_tipo_registro = c_tipo_registro;                
+	this.c_data_do_pregao = c_data_do_pregao;              
+	this.c_codigo_bdi = c_codigo_bdi;                   
 	this.c_codigo_de_negociacao_do_papel = c_codigo_de_negociacao_do_papel;
-	this.c_tipo_de_mercado = c_tipo_de_mercado;
-	this.c_nomres = c_nomres;
-	this.c_especificacao_do_papel = c_especificacao_do_papel;
-	this.c_prazot = c_prazot;
-	this.c_moeda_de_referencia = c_moeda_de_referencia;
-	this.c_preabe = c_preabe;
-	this.c_premax = c_premax;
-	this.c_premin = c_premin;
-	this.c_premed = c_premed;
-	this.c_preult = c_preult;
-	this.c_preofc = c_preofc;
-	this.c_preofv = c_preofv;
-	this.c_totneg = c_totneg;
-	this.c_quatot = c_quatot;
-	this.c_voltot = c_voltot;
-	this.c_preexe = c_preexe;
-	this.c_idopc = c_idopc;
-	this.c_datven = c_datven;
-	this.c_fatcot = c_fatcot;
-	this.c_ptoexe = c_ptoexe;
-	this.c_codisi = c_codisi;
+	this.c_tipo_de_mercado = c_tipo_de_mercado;              
+	this.c_nomres = c_nomres;                       
+	this.c_especificacao_do_papel = c_especificacao_do_papel;       
+	this.c_prazot = c_prazot;                       
+	this.c_moeda_de_referencia = c_moeda_de_referencia;          
+	this.c_preabe = c_preabe;                       
+	this.c_premax = c_premax;                       
+	this.c_premin = c_premin;                       
+	this.c_premed = c_premed;                       
+	this.c_preult = c_preult;                       
+	this.c_preofc = c_preofc;                       
+	this.c_preofv = c_preofv;                       
+	this.c_totneg = c_totneg;                       
+	this.c_quatot = c_quatot;                       
+	this.c_voltot = c_voltot;                       
+	this.c_preexe = c_preexe;                       
+	this.c_idopc  = c_idopc ;                       
+	this.c_datven = c_datven;                       
+	this.c_fatcot = c_fatcot;                       
+	this.c_ptoexe = c_ptoexe;                       
+	this.c_codisi = c_codisi;                       
 	this.c_dismes = c_dismes;
+  }
+};
+
+class bovespaTrailer {
+	constructor(t_tipo_registro, t_nome_arquivo, t_codigo_da_origem, t_data_geracao_do_arquivo, t_total_registros, t_reserva) {
+	this.t_tipo_registro = t_tipo_registro;
+	this.t_nome_arquivo = t_nome_arquivo;
+	this.t_codigo_da_origem = t_codigo_da_origem;
+	this.t_data_geracao_do_arquivo = t_data_geracao_do_arquivo;
+	this.t_total_registros = t_total_registros;
+	this.t_reserva = t_reserva;
   }
 };
 
 var bovespaHeaderData = bovespaHeaderData || [];
 var bovespaCotacaoData = bovespaCotacaoData || [];
+var bovespaTrailerData = bovespaTrailerData || [];
 
 fs.readFile('resources/COTAHIST_M122016.TXT', function (err, data) {
    
@@ -84,8 +97,8 @@ fs.readFile('resources/COTAHIST_M122016.TXT', function (err, data) {
 		
 		/*Fazendo a leitura do Cotações*/
 		else if (line.substring(0, 2) == '01'){
-			var c_tipo_registro = line.substring(0, 2);
-			var c_data_do_pregao = line.substring(2, 10);
+			var c_tipo_registro = parseInt(line.substring(0, 2));
+			var c_data_do_pregao = parseInt(line.substring(2, 10));
 			var c_codigo_bdi = line.substring(10, 12);
 			var c_codigo_de_negociacao_do_papel = line.substring(12, 24);
 			var c_tipo_de_mercado = line.substring(24, 27);
@@ -93,7 +106,7 @@ fs.readFile('resources/COTAHIST_M122016.TXT', function (err, data) {
 			var c_especificacao_do_papel = line.substring(39, 49);
 			var c_prazot = line.substring(49, 52);
 			var c_moeda_de_referencia = line.substring(52, 56);
-			var c_preabe = line.substring(56, 69);
+			var c_preabe = (parseInt((line.substring(56, 69)))/100).toFixed(2);
 			var c_premax = line.substring(69, 82);
 			var c_premin = line.substring(82, 95);
 			var c_premed = line.substring(95, 108);
@@ -111,9 +124,8 @@ fs.readFile('resources/COTAHIST_M122016.TXT', function (err, data) {
 			var c_codisi = line.substring(230, 242);
 			var c_dismes = line.substring(242, 245);
 
-			var bovespac = new bovespaCotacao(c_tipo_registro, c_data_do_pregao, c_codigo_bdi, c_codigo_de_negociacao_do_papel, c_tipo_de_mercado, c_nomres, c_especificacao_do_papel, c_prazot, c_moeda_de_referencia, c_preabe, c_premax, c_premin, c_premed, c_preult, c_preofc, c_preofv, c_totneg, c_quatot, c_voltot, c_preexe, c_idopc, c_datven, c_fatcot, c_ptoexe, c_codisi, c_dismes);
+			var bovespac = new bovespaCotacao(c_tipo_registro, c_data_do_pregao, c_codigo_bdi, c_codigo_de_negociacao_do_papel, c_tipo_de_mercado, c_nomres, c_especificacao_do_papel, c_prazot, c_moeda_de_referencia, c_preabe, c_premax, c_premin, c_premed, c_preult, c_preofc, c_preofv, c_totneg, c_quatot, c_voltot, c_preexe, c_idopc , c_datven, c_fatcot, c_ptoexe, c_codisi, c_dismes);
 			bovespaCotacaoData.push(bovespac);
-
 		}
 
 		/*Fazendo a leitura do Trailer*/
@@ -124,6 +136,9 @@ fs.readFile('resources/COTAHIST_M122016.TXT', function (err, data) {
 			var t_data_geracao_do_arquivo = line.substring(23, 31);
 			var t_total_registros = parseInt(line.substring(31, 42));
 			var t_reserva = line.substring(42, 245);
+
+			var bovespat = new bovespaTrailer(t_tipo_registro, t_nome_arquivo, t_codigo_da_origem, t_data_geracao_do_arquivo, t_total_registros, t_reserva);
+			bovespaTrailerData.push(bovespat);
 		}
 
 	});
@@ -136,6 +151,14 @@ app.get ('/', function (request, response){
 
 app.get('/dataHeader', function(request, response){
 	response.send({bovespaHeaderData});
+});
+
+app.get('/dataTrailer', function(request, response){
+	response.send({bovespaTrailerData});
+});
+
+app.get('/dataCotacao', function(request, response){
+	response.send({bovespaCotacaoData});
 });
 
 app.listen (3000, function(){
--- views/scripts/bovespa.js
+++ views/scripts/bovespa.js
@@ -1,13 +1,33 @@
 /*cria um module e direciona para um controlador */
-angular.module('bovespaApp', []).controller('bovespaListController', ['$http', '$scope', function($http, $scope){
+angular.module('bovespaApp', ['angularUtils.directives.dirPagination']).controller('bovespaListController', ['$http', '$scope', function($http, $scope){
   /*Aponta todoList para o this que é o controlador na view*/
   var bovespaList = this;
   
-  /* Buscar Informações da rota /data no servidor */
+  /* Buscar Informações da rota /dataHeader no servidor */
   $http({method: 'GET', url: '/dataHeader'}).then(function successCallback(data) {
       
       /* Recebe o objeto GET por /data */
-      $scope.data = data.data;
+      $scope.dataHeader = data.data;
+
+  }, function errorCallback(response) {
+    console.log('Erro ao receber arquivo: ' + data);
+  });
+
+  /* Buscar Informações da rota /dataCotacao no servidor */
+  $http({method: 'GET', url: '/dataCotacao'}).then(function successCallback(data) {
+      
+      /* Recebe o objeto GET por /data */
+      $scope.dataCotacao = data.data;
+
+  }, function errorCallback(response) {
+    console.log('Erro ao receber arquivo: ' + data);
+  });
+
+  /* Buscar Informações da rota /dataTrailer no servidor */
+  $http({method: 'GET', url: '/dataTrailer'}).then(function successCallback(data) {
+      
+      /* Recebe o objeto GET por /data */
+      $scope.dataTrailer = data.data;
 
   }, function errorCallback(response) {
     console.log('Erro ao receber arquivo: ' + data);
